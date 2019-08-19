21e744d068f8bc3735715d18f44ef3033aeb0839
WonDongGyun <waterflame1@naver.com>
2019-08-19 13:50:30 +0900

    My Django Girls app, second commit

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.conf.urls import url
+from . import views
+from django.conf import settings # add
+from django.conf.urls.static import static # add
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    url(r'^uimage/$', views.uimage, name='uimage'), # add
+]
+ 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect
+from .forms import UploadImageForm
+from django.core.files.storage import FileSystemStorage
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def uimage(request):
+  if request.method == 'POST':
+      form = UploadImageForm(request.POST, request.FILES)
+      if form.is_valid():
+          myfile = request.FILES['image']
+          fs = FileSystemStorage()
+          filename = fs.save(myfile.name, myfile)
+          uploaded_file_url = fs.url(filename)
+          return render(request, 'blog/uimage.html', {'form': form, 'uploaded_file_url': uploaded_file_url})
+  else:
+      form = UploadImageForm()
+      return render(request, 'blog/uimage.html', {'form': form})
